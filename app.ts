d584dc33ccc855c14d50c54d2168cf46e15d00c1
M Hamza Bhatti <itshamzabhatti@gmail.com>
2024-04-26 16:57:00 +0500

    new commit

---
+++ app.ts
@@ -0,0 +1,21 @@
+/*
+git  //to check the common commands of git
+git init  //to initialize git so it will mark U(untracked) on file.
+git add Read.md   //to track the md(mark down) file. so it will change the U sign to A(Active)
+git add .    //to Active all the Untracked file in one command
+git status  //to check the git status either commet, tracked done or not
+git commit -m "first commit"  //to add commit on git
+git branch   //to check the branch 
+git branch -M main    //to change the branch from master to main
+git config --global user.name "hamza"  //to add user name
+git config --global user.email "hamza@gmail.com"  //to add email
+git remote add origin https://github.com/M-Hamza-Bhatti/new-practice.git  //to remote add the file on github  
+git push -u origin main  //to upload or push the file to github
+git push origin main  //to check every work upto date , uploaded or not
+git pull origin main  //to add those file on vs-code which are made on github.
+npm init -y   // to add package.json file
+npm i @types/node -D  //to make modulus folder and a lock.json file 
+
+*/
+
+console.log("Hello world")
